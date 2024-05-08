8508a87827e09fb348aeaab746492d5559ef287c
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-05-08 16:27:55 +0800

    movieflix-react study

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+
+import "./App.css"
+import SearchIcon from "./search.svg"
+import MovieCard from "./MovieCard";
+
+const API_URL = 'https://www.omdbapi.com?apikey=bf9758c3';
+
+
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies("spiderman");
+    }, []);
+
+    return(
+        <div className="app">
+            <h1>Honeygailflix</h1>  
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={(e) => {
+                       if(e.key === "Enter") searchMovies(e.target.value)
+                    }}
+                />
+                
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                            {movies.map((movie) => (
+                                <MovieCard movie={movie}/>
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No Movies Found</h2>
+                        </div>
+                    )
+            }
+        </div>
+    )
+}
+
+export default App;
