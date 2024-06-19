05057e16e3057b9b36db0baab48f4381e102fab7
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-20 03:59:10 +0800

    Restructure of react app

--- src/App.js
+++ src/App.js
@@ -1,66 +1,13 @@
-import { useEffect, useState } from "react";
-
+import React from "react";
 import "./App.css"
-import SearchIcon from "./search.svg"
-import MovieCard from "./MovieCard";
-
-const API_URL = 'https://www.omdbapi.com?apikey=bf9758c3';
-
-
+import { Movie } from "./components/Movie/Movie";
+import { Navbar } from "./components/Navbar/Navbar";
 
 const App = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
-
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-        
-
-
-        console.log(data)
-        setMovies(data.Search);
-    }
-
-    useEffect(() => {
-        searchMovies("spiderman");
-    }, []);
-
     return(
         <div className="app">
-            <h1>Honeygailflix</h1>  
-
-            <div className="search">
-                <input
-                    placeholder="Babe, search for movies here"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    onKeyDown={(e) => {
-                       if(e.key === "Enter") searchMovies(e.target.value)
-                    }}
-                />
-                
-                <img
-                    src={SearchIcon}
-                    alt="search"
-                    onClick={() => searchMovies(searchTerm)}
-                />
-            </div>
-
-            {
-                movies?.length > 0
-                    ? (
-                        <div className="container">
-                            {movies.map((movie) => (
-                                <MovieCard movie={movie}/>
-                            ))}
-                        </div>
-                    ) : (
-                        <div className="empty">
-                            <h2>No Movies Found</h2>
-                        </div>
-                    )
-            }
+            <Navbar />
+            <Movie />
         </div>
     )
 }
