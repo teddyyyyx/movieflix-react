6cdcd0ae8fbbf536c1aeef6feb0eb977fdc0a071
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-20 04:00:49 +0800

    Add MovieProvider around App/

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 import App from './App.js';
+import MovieProvider from "./Context/MovieContext.jsx";
 
-// ReactDOM.render(<App />, document.getElementById('root'))
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <MovieProvider>
       <App />
-    </React.StrictMode>,
+    </MovieProvider>,
   )
   
