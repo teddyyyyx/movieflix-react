44f4ff3f72ab409f86a01ab70cc380752cba6b95
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-20 17:24:53 +0800

    Added global css variables

--- src/Context/MovieContext.jsx
+++ src/Context/MovieContext.jsx
@@ -6,7 +6,7 @@ const API_URL = 'https://www.omdbapi.com?apikey=bf9758c3';
 const MovieProvider = ({ children }) => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
-
+    const [currentSearch, setCurrentSearch] = useState("avengers")
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
@@ -14,10 +14,10 @@ const MovieProvider = ({ children }) => {
     }
 
     useEffect(() => {
-        searchMovies("spiderman");
+        searchMovies("avengers");
     }, []);
 
-    const contextValue = { movies, searchTerm, setSearchTerm, searchMovies};
+    const contextValue = { movies, searchTerm, setSearchTerm, searchMovies, currentSearch, setCurrentSearch};
 
     return (
         <MovieContext.Provider value={contextValue}>
