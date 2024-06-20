f5e047388e9d30544582946b07aa61d03dd60687
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-20 11:50:57 +0800

    Moved to components folder

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,26 +1,26 @@
 import React, { useContext } from 'react'
 import { MovieContext } from '../../Context/MovieContext'
-import MovieCard from '../../MovieCard';
+import MovieCard from '../MovieCard/MovieCard';
 import './movie.css'
 export const Movie = () => {
     const {movies} = useContext(MovieContext);
 
   return (
-    <div>           
+        <>           
             {
-        movies?.length > 0
-            ? (
-                <div className="container">
-                    {movies.map((movie) => (
-                        <MovieCard movie={movie}/>
-                    ))}
-                </div>
-            ) : (
-                <div className="empty">
-                    <h2>No Movies Found</h2>
-                </div>
-            )
-    }
-        </div>
+                movies?.length > 0
+                ? (
+                    <div className="movie-container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No Movies Found</h2>
+                    </div>
+                )
+            }
+        </>
   )
 }
