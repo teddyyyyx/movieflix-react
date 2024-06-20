68a9c74b5d2251e6de03b48495c99f73e323bea8
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-20 17:32:33 +0800

    Changed src/Context into src/context (lowercase)

---
+++ src/context/MovieContext.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+export const MovieContext = createContext(null);
+const API_URL = 'https://www.omdbapi.com?apikey=bf9758c3';
+
+const MovieProvider = ({ children }) => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [currentSearch, setCurrentSearch] = useState("avengers")
+    const [movieDetails, setMovieDetails] = useState(null);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies("avengers");
+    }, []);
+
+    const fetchMovieDetails = async (imdbID) => {
+        const response = await fetch(`${API_URL}&i=${imdbID}`);
+        const data = await response.json();
+        setMovieDetails(data);
+    }
+
+    const contextValue = { movies, searchTerm, setSearchTerm, searchMovies, currentSearch, setCurrentSearch, movieDetails, fetchMovieDetails};
+
+    return (
+        <MovieContext.Provider value={contextValue}>
+            {children}
+        </MovieContext.Provider>
+    )
+}
+
+export default MovieProvider;
