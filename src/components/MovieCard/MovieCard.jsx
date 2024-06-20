c28966fa1f043f5686e14547f2c829f2379d23cf
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-20 17:30:22 +0800

    Updated MovieCard component UI

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import honeyImg from '../../assets/honey.jpeg'
 import './movieCard.css'
+
 const MovieCard = ({movie}) => {
 
     return (
-        <div className="movie">
-            <div>
-                <p>{movie.Year}</p>
-            </div>
+        <div>
+            <Link to={`/movie/${movie.imdbID}`} >
+                <div className="movie">
+                    <div>
+                        <p>{movie.Year}</p>
+                    </div>
 
-            <div>
-                <img src={movie.Poster} alt="movie1.Title"/>
-            </div>
+                    <div>
+                        <img src={movie.Poster  !== 'N/A' ? movie.Poster: honeyImg} alt={movie.Title} />
+                    </div>
 
-            <div>
-            <span>{movie.Type}</span>
-            <h3 className="movie-title">{movie.Title}</h3>
-            </div>
+                    <div>
+                    <span>{movie.Type}</span>
+                    <h3 className="movie-title">{movie.Title}</h3>
+                    </div>
+                </div>
+            </Link>
         </div>
     )
 }
