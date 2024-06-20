772e96fa4a813f01470c5473280f2fddc44d6942
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-20 17:27:00 +0800

    Added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 import App from './App.js';
-import MovieProvider from "./Context/MovieContext.jsx";
+import MovieProvider from "./context/MovieContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <MovieProvider>
-      <App />
-    </MovieProvider>,
+    <BrowserRouter>
+      <MovieProvider>
+        <App />
+      </MovieProvider>,
+    </BrowserRouter>
   )
   
