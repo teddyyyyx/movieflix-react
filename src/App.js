05d4f41159b21d7eb651f74f89aa76401a5e8f7d
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-20 13:28:38 +0800

    Restruction Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 import { Movie } from "./components/Movie/Movie";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
 import { Navbar } from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
-    return(
-        <div className="app">
-            <Navbar />
-            <Movie />
-        </div>
-    )
-}
+    return (
+            <div className="app">
+                <Navbar />
+                <Routes>
+                    <Route exact path="/" element={<Movie />} />
+                    <Route exact path="/movie/:imdbID" element={<MovieDetails />} />
+                </Routes>
+            </div>
+    );
+};
 
 export default App;
-
-
