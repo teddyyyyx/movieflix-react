87fca1e95bf807022cef2bb2b94bfbbacc58c2ae
Benz Samson Tagle <benz@teddyyyyx.bbrouter>
2024-06-20 17:26:19 +0800

    Added BrowserRouter and footer component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import { Movie } from "./components/Movie/Movie";
 import MovieDetails from "./components/MovieDetails/MovieDetails";
 import { Navbar } from "./components/Navbar/Navbar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { Footer } from "./components/Footer/Footer";
 
 const App = () => {
     return (
@@ -13,6 +14,7 @@ const App = () => {
                     <Route exact path="/" element={<Movie />} />
                     <Route exact path="/movie/:imdbID" element={<MovieDetails />} />
                 </Routes>
+                <Footer />
             </div>
     );
 };
